497200a67b7335412a7b69c3442fe1ca703b0135
Alok <designedbyalok@gmail.com>
2023-05-12 20:48:34 +0530

    init commit

---
+++ src/bio.js
@@ -0,0 +1,44 @@
+export const bio = {
+  name: "Alok Kumar",
+  shortName: "Alok",
+  image: "/public/Alok.jpeg",
+  designation: "Product Designer",
+  email: "designedbyalok@gmail.com",
+  about:
+    " a Product Designer in Bengaluru ,India. Currently busy bringing consumer level ease of use to enterprise products.",
+  socials: [
+    {
+      name: "LinkedIn",
+      icon: "/public/linkedin.svg",
+      url: "https://www.linkedin.com/in/designedbyalok/",
+    },
+    {
+      name: "Dribbble",
+      icon: "/public/dribbble.svg",
+      url: "https://dribbble.com/designedbyalok",
+    },
+    {
+      name: "Medium",
+      icon: "/public/medium.svg",
+      url: "https://medium.com/@designedbyalok",
+    },
+  ],
+  tabs: [
+    {
+      name: "Blogs",
+      url: "",
+    },
+    {
+      name: "Works",
+      url: "https://linktr.ee/designedbyalok",
+    },
+    {
+      name: "Resume",
+      url: "https://www.dropbox.com/s/6azss1fz4e22pxo/Alok_Kumar-compressed.pdf?dl=0",
+    },
+    {
+      name: "Contact",
+      url: "",
+    },
+  ],
+};
